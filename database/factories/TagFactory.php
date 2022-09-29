a2db44fc7e5abf7053614e76f26eeba3a942c00b
Joel G <wanstiller@gmail.com>
2022-09-28 22:15:40 -0500

    Subiendo Datos

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -1,8 +1,13 @@
 <?php
 
+
 namespace Database\Factories;
 
+Use App\Models\Tag;
+
 use Illuminate\Database\Eloquent\Factories\Factory;
+
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
@@ -16,8 +21,12 @@ class TagFactory extends Factory
      */
     public function definition()
     {
+
+        $name = $this->faker->unique()->word(20);
+
         return [
-            //
+            'name' => $name,
+            'slug' => Str::slug($name)
         ];
     }
 }
--- database/migrations/2022_09_16_050510_create_images_table.php
+++ database/migrations/2022_09_16_050510_create_images_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('url');
-
             $table->unsignedBigInteger('imageable_id');
             $table->string('imageable_type');
             $table->timestamps();
